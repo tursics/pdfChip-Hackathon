b3cc52b088e285a61c7b5e26ce613a1f2a10df83
Thomas Tursics <thomas@tursics.de>
2015-11-23 21:38:45 +0100

    create a simple file for the base link

---
+++ scripts/link.js
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------
+// Example JavaScript document
+//---------
+// Copyright: © 2014 - David van Driessche, Four Pees, and some more
+//-----------------------------------------------------------------------
+
+//var baseLink = 'https://nl.wikipedia.org/wiki/Pingu%C3%AFns';
+//var baseLink = 'https://en.wikipedia.org/wiki/Penguin';
+//var baseLink = 'https://de.wikipedia.org/wiki/Olaf_Dr%C3%BCmmer';
+var baseLink = 'https://nl.wikipedia.org/wiki/Portaal:Vogels';
+
+//var baseLink = 'https://en.wikipedia.org/wiki/Bird';
+
+//-----------------------------------------------------------------------
